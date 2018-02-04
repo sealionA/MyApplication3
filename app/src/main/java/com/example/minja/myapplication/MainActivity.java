112b9c8077ce5e72d670b2673f4c0925c71052df
MinJae Kim <minjae0456@naver.com>
2018-02-04 21:07:37 +0900

    PDF암묵적 파일 열기
    PDF를 암묵적으로 여는 프로그램

--- app/src/main/java/com/example/minja/myapplication/MainActivity.java
+++ app/src/main/java/com/example/minja/myapplication/MainActivity.java
@@ -1,32 +1,60 @@
 package com.example.minja.myapplication;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText)findViewById(R.id.editText);
+        editText = (EditText) findViewById(R.id.editText);
+    }
 
-        Button button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view)
-            {
-                String data = editText.getText().toString();
 
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(data));
-                startActivity(intent);
+      public void onButton1Clicked(View v) {
+           String filename = editText.getText().toString();
+           if (filename.length() > 0) {
+               openPDF(filename.trim());
+           } else {
+                Toast.makeText(getApplicationContext(), "PDF 파일명을 입력하십시오.", Toast.LENGTH_LONG).show();
+          }
+      }
+
+     public void openPDF(String filename) {
+            String sdcardFolder = Environment.getExternalStorageDirectory().getAbsolutePath();
+          String filepath = sdcardFolder + File.separator + filename;
+          File file = new File(filepath);
+
+         Toast.makeText(this,sdcardFolder,Toast.LENGTH_LONG).show();
+
+          if (file.exists()) {
+             Uri uri = Uri.fromFile(file);
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(uri, "application/pdf");
+
+              try {
+                  startActivity(intent);
+             } catch (ActivityNotFoundException ex) {
+                    Toast.makeText(this, "PDF 파일을 보기 위한 뷰어 앱이 없습니다.", Toast.LENGTH_SHORT).show();
+
             }
-        });
+        }
     }
 }
+
+
